3518122355b7ece416c1a4ae1d57ca30ea35809e
Islam Nurlanov <islamnnurlanov@mail.ru>
2019-05-19 20:24:11 +0600

    Creata class modal and style

--- src/Akci.js
+++ src/Akci.js
@@ -12,6 +12,17 @@ const Styles = styled.div `
         border:1px solid black
         
 }
+.modal{
+        display:none;
+        position:fixed;
+        z-index:1;
+        left:0;
+        top:0;
+        width:100%;
+        height:100%;
+        overflow:auto;
+        background-color:rgba(0,0,0,0.8);
+}
 
 `;
 
